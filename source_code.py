32994063a7a4156f707315fb47e508dae272f531
ApongsehIyan23 <apongsehiyan23@gmail.com>
2026-02-03 00:45:12 +0200

    Modified the source_code.py for it to scan regex patterns by paragraphs instead of lines

--- source_code.py
+++ source_code.py
@@ -4,11 +4,11 @@ import json
 #Regexes
 
 validators = {
-    "email": r"^(?!.*[\r\n])(?!.*[<>])(?=.{5,255}$)\w[\w.-]{0,63}@[\w.-]{1,192}\.[a-zA-Z]{2,63}$",
-    "phone": r"^(?!.*[\r\n])(?!.*[<>])(?=.{16}$)\+237\s[623]\d{2}-\d{3}-\d{3}$",
-    "credit_card": r"^(?!.*[\r\n])(?!.*[<>])(?=.{19}$)(\d{4})-\d{4}-\d{4}-(\d{4})$",
-    "time": r"^(?!.*[\r\n])(?!.*[<>])(?=.{5}$)([01]\d|2[0-3]):[0-5]\d$",
-    "currency": r"^(?!.*[\r\n])(?!.*[<>])(?=.{4,15}$)\$\d{1,3}(,\d{3})*\.\d{2}$"
+    "email": r"(?!.*[\r\n])(?!.*[<>])(?=.{5,255}$)\w[\w.-]{0,63}@[\w.-]{1,192}\.[a-zA-Z]{2,63}",
+    "phone": r"(?!.*[\r\n])(?!.*[<>])(?=.{16}$)\+237\s[623]\d{2}-\d{3}-\d{3}",
+    "credit_card": r"(?!.*[\r\n])(?!.*[<>])(?=.{19}$)(\d{4})-\d{4}-\d{4}-(\d{4})",
+    "time": r"(?!.*[\r\n])(?!.*[<>])(?=.{5}$)([01]\d|2[0-3]):[0-5]\d",
+    "currency": r"(?!.*[\r\n])(?!.*[<>])(?=.{4,15}$)\$\d{1,3}(,\d{3})*\.\d{2}"
 }
 
 # This dictionary will store our clean data
@@ -25,23 +25,20 @@ with open('sample_input.txt', 'r') as file:
     lines = file.readlines()
 
 for line in lines:
-    clean_line = line.strip() # Removes invisible spaces and \n
-    
-    if not clean_line:
-        continue # Skip empty lines
-
-    # 3. THE LOOP: CHECK EVERY LINE AGAINST EVERY REGEX
     for category, pattern in validators.items():
-        if re.match(pattern, clean_line):
+        # finding the data in the paragraphs
+        match = re.search(pattern, line.strip())
+        
+        if match:
+            found_data = match.group(0)
             
-            # Mask the Credit Card for privacy
-            if category == "credit_card":
-                masked_card = re.sub(pattern, r"\1-****-****-\2", clean_line)
-                extracted_results[category].append(masked_card)
-            else:
-                extracted_results[category].append(clean_line)
-            
-            break # Move to next line once we find a match
+            # Another check to ensure no unwanted characters
+            if not re.search(r"[\r\n<>]", found_data):
+                if category == "credit_card":
+                    masked = re.sub(pattern, r"\1-****-****-\2", found_data)
+                    extracted_results[category].append(masked)
+                else:
+                    extracted_results[category].append(found_data)
 
 # 4. SAVE TO JSON
 with open('sample_output.json', 'w') as f:
